27a09c76bd43f3d62818b5720305db0ab2fe748d
tito <107213851+migueltitoxd@users.noreply.github.com>
2023-09-13 13:31:07 -0500

    create alumnos

--- colegio/alumnos/forms.py
+++ colegio/alumnos/forms.py
@@ -5,8 +5,27 @@ from users.models import CustomUserAlumno
 class CustomUserAlumnoForm(UserCreationForm):
     class Meta:
         model = CustomUserAlumno
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'foto', 'tipo_documento', 'descripcion', 'introduccion', 'numero_documento', 'tipo_usuario', 'grado', 'sexo', 'estado', 'alergias', 'condiciones_medicas', 'medicamentos_actuales', 'grupo_sanguineo', 'contacto_emergencia_nombre', 'contacto_emergencia_telefono', 'ver_notas')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password','tipo_documento', 'grado', 'sexo')
 
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5', 'placeholder': 'Nombre del usuario'}),
+
+            'first_name': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5', 'placeholder': 'Apellido del usuario'}),
+
+            'last_name': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5', 'placeholder': 'Numero de telefono del usuario'}),
+
+
+            'tipo_documento': forms.Select(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5'}),
+
+            'password': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5', 'placeholder': 'Numero de telefono del usuario'}),
+
+            'email': forms.EmailInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5', 'placeholder': 'Email donde se puede contactar al usuario'}),
+
+            'grado': forms.Select(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5'}),
+
+            'sexo': forms.Select(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5'}),
+        }
+
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Personaliza los widgets o agrega campos adicionales si es necesario
