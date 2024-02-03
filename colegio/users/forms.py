55891beb974b0532e201af560bb6df4869b3aae3
Miguel Angel Diaz <miguelitodiaz169@gmail.com>
2024-02-03 12:29:49 -0500

    formulario de colegios estilizado

--- colegio/users/forms.py
+++ colegio/users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from allauth.account.forms import SignupForm, LoginForm
 from .models import Colegio
+from django.forms import ClearableFileInput
+
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
@@ -16,6 +18,14 @@ class ColegioForm(forms.ModelForm):
     class Meta:
         model = Colegio
         fields = ['clave', 'colegio', 'numero', 'direccion', 'descripcion', 'foto', 'banner']
-        widgets = {}
+        widgets = {
+            'clave': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5', 'placeholder': 'Nombre Clave'}),
+            'colegio': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5', 'placeholder': 'Nombre del colegio'}),
+            'numero': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5', 'placeholder': 'Numero de contacto'}),
+            'direccion': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5', 'placeholder': 'Direccion del colegio'}),
+            'descripcion': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5', 'placeholder': 'Descripcion del colegio'}),
+            'foto': ClearableFileInput(attrs={ "class":"block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 p-2.5", "id":"input_file", "type":"file"}),
+            'banner': ClearableFileInput(attrs={ "class":"block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 p-2.5", "id":"input_file", "type":"file"}),
+        }
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
