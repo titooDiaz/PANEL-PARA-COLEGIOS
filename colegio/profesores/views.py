9d8bf5edba7d49de193c1746c97c169bc7366077
Miguel Diaz <miguelitodiaz169@gmail.com>
2024-06-21 19:41:28 -0500

    filtrado por profesor de materias

--- colegio/profesores/views.py
+++ colegio/profesores/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from informacion.models import Materias, Grado
 
 class BoardProfesores(View):
     def get(self, request, *args, **kwargs):
-        vista = 'gestor'
+        vista = 'profesores'
         abierto='inicio'
+        profesor = request.user.pk
+        materias_profesor = Materias.objects.filter(profe1=profesor) | Materias.objects.filter(profe2=profesor)
+        grados = Grado.objects.filter(materias__in=materias_profesor).distinct()
         context = {
+            'grados': grados,
+            'materias_profesor': materias_profesor,
             'vista': vista,
             'abierto':abierto,
         }
