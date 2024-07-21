ba3b5ef894b5eef30cb2d98830e9455ec9313a86
Miguel Diaz <miguelitodiaz169@gmail.com>
2024-07-21 11:26:34 -0500

    create view of 'EditActivities'

--- colegio/profesores/urls.py
+++ colegio/profesores/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('board/', BoardProfesores.as_view(), name='BoardProfesores'),
-    path('view/actividades/<int:pk>', ViewActividades.as_view(), name='ViewActividades'),
+    path('actividades/create/<int:pk>', CreateActividades.as_view(), name='CreateActividades'),
+    path('edit/actividades/<int:pk>', EditActividades.as_view(), name='EditActividades'),
 ]
--- colegio/profesores/views.py
+++ colegio/profesores/views.py
@@ -54,7 +54,59 @@ def get_midnight(user):
 
 
 
-class ViewActividades(View):
+class CreateActividades(View):
+    def get(self, request, pk, *args, **kwargs):
+        vista = 'profesores'
+        abierto='inicio'
+        materia = Materias.objects.get(pk=pk)
+        grado = Grado.objects.get(materias=materia)
+
+        initial_data = {
+            'fecha_inicio': get_current_date(request.user),
+            'fecha_final': get_current_date(request.user),
+            'hora_inicio': get_current_time(request.user),
+            'hora_final': get_midnight(request.user),
+        }
+        actividades_form = ActividadesForm(initial=initial_data)
+        print(materia.titulo1,"  -  ")
+        context = {
+            'materia': materia,
+            'grado': grado,
+            'actividades': actividades_form,
+            'vista': vista,
+            'abierto':abierto,
+        }
+        return render(request, 'users/profesores/actividades/create_actividades.html', context)
+    def post(self, request, pk, *args, **kwargs):
+        actividades_form = ActividadesForm(request.POST)
+        #archivo_form = ArchivoForm(request.POST, request.FILES)
+
+        if actividades_form.is_valid():
+        # Crear una instancia del modelo sin guardar en la base de datos aún
+            actividad = actividades_form.save(commit=False)
+            actividad.author = request.user  # Asigna el usuario actual como autor
+
+            try:
+                materia_colegio = Materias.objects.get(pk=pk)
+                actividad.materia = materia_colegio  # Asigna la materia a la actividad
+                
+                # Guarda la instancia del modelo en la base de datos
+                actividad.save()
+                messages.success(request, 'Actividad agregada correctamente!')
+            except Materias.DoesNotExist:
+                messages.error(request, 'La materia especificada no existe')
+                return redirect('BoardProfesores')
+            except TypeError:
+                messages.error(request, 'Verifica tus datos')
+                return redirect('BoardProfesores')
+            
+            return redirect('BoardProfesores')
+
+        # Si el formulario no es válido, muestra los errores
+        messages.error(request, 'Formulario no válido')
+        return redirect('BoardProfesores')
+    
+class EditActividades(View):
     def get(self, request, pk, *args, **kwargs):
         vista = 'profesores'
         abierto='inicio'
