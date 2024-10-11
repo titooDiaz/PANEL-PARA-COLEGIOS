894fd2123b7bd815f818aa623c7400dd30fb102c
Miguel Diaz <miguelitodiaz169@gmail.com>
2024-10-11 09:04:13 -0500

    list of uploads files

--- colegio/static/js/drop_input.js
+++ colegio/static/js/drop_input.js
@@ -1,5 +1,6 @@
 const dropArea = document.getElementById('drop-area');
 const fileInput = document.getElementById('archivo');
+const filesList = document.getElementById('files_list'); // lista de archivos subidos
 
 // Prevenir comportamientos por defecto para eventos de arrastrar y soltar
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
@@ -29,6 +30,15 @@ function handleDrop(e) {
     const files = dt.files;
     fileInput.files = files;  // Asignar los archivos al input de tipo file
     console.log("Archivo cargado en el formulario:", fileInput.files[0]);  // Opcional: ver el archivo en la consola
+
+    // Crear un elemento HTML e insertarlo en el DOM
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="p-4 flex w-full bg-green-400 rounded-lg my-4">
+                        <div><i class="fa-solid fa-file"></i></div>
+                        <div class="ml-4">${files[0].name}</div>
+                    </div>`;
+    
+    filesList.appendChild(div);
 }
 
 // SUBIDO MANUALMNTE
@@ -37,4 +47,14 @@ fileInput.addEventListener('change', function() {
     if (files.length > 0) {
         console.log("Archivo seleccionado:", files[0]);  // Muestra el nombre del archivo seleccionado
     }
+
+    // Crear un elemento HTML e insertarlo en el DOM
+    const div = document.createElement('div');
+    div.innerHTML = `
+                    <div class="p-4 flex w-full bg-green-400 rounded-lg my-4 ">
+                        <div><i class="fa-solid fa-file"></i></div>
+                        <div class="ml-4">${files[0].name}</div>
+                    </div>`;
+    
+    filesList.appendChild(div);
 });
