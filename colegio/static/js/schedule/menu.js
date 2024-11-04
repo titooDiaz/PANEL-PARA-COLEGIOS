07d9b8a9f26c22e30d420fbad7e296ff94255860
Miguel Diaz <miguelitodiaz169@gmail.com>
2024-11-04 12:00:01 -0500

    schedule of phones

--- colegio/static/js/schedule/menu.js
+++ colegio/static/js/schedule/menu.js
@@ -1,8 +1,13 @@
-
 function showSection(section) {
     // Ocultar todas las secciones
     document.querySelectorAll('[id^="content_schedule"] > div').forEach(div => div.classList.add('hidden'));
     
     // Mostrar la sección seleccionada
+    // show the select section
     document.getElementById(section).classList.remove('hidden');
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // Desplazar al elemento de ancla
+    document.getElementById('top').scrollIntoView({ behavior: 'smooth' });
+
+
 }
